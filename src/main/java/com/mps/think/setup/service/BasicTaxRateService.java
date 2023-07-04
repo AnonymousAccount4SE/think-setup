8ca9afcd4c29ee2bbe7b6ad7da2830bf33f1405b
Samir Anand <Samir.Anand@macmill.com>
2023-07-04 12:36:14 +0530

    add

--- src/main/java/com/mps/think/setup/service/BasicTaxRateService.java
+++ src/main/java/com/mps/think/setup/service/BasicTaxRateService.java
@@ -1,8 +1,7 @@
 package com.mps.think.setup.service;
 
-import com.mps.think.setup.model.AddEffort;
 import com.mps.think.setup.model.BasicTaxRate;
-import com.mps.think.setup.vo.AddEffortVO;
+
 import com.mps.think.setup.vo.BasicTaxRateVO;
 
 public interface BasicTaxRateService {
