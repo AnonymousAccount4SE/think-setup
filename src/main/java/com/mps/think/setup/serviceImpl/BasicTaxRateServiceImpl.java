9fea4ffac18032012850b054b562b73e505f235e
Ranjit-Yadav <75121438+Ranjit-Yadav@users.noreply.github.com>
2023-07-04 11:52:46 +0530

    update BasicTaxRateServiceImpl

--- src/main/java/com/mps/think/setup/serviceImpl/BasicTaxRateServiceImpl.java
+++ src/main/java/com/mps/think/setup/serviceImpl/BasicTaxRateServiceImpl.java
@@ -1,10 +1,7 @@
 package com.mps.think.setup.serviceImpl;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.webjars.NotFoundException;
 
 import com.mps.think.setup.model.BasicTaxRate;
 import com.mps.think.setup.repo.BasicTaxRateRepo;
@@ -17,11 +14,8 @@ public class BasicTaxRateServiceImpl implements BasicTaxRateService {
 
 	@Override
 	public BasicTaxRate findbasicTaxRatebyId(Integer id) {
-		Optional<BasicTaxRate> jl = basicTaxRateRepo.findById(id);
-		if (!jl.isPresent()) {
-			throw new NotFoundException("BasicTaxRate Id : " + id + " does not exist!");
-		}
-		return jl.get();
+		return basicTaxRateRepo.findById(id).get();
+		
 	}
 
 }
