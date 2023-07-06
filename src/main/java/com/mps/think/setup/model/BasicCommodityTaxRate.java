0b1c9f5ec31466740c9f39f55df4b76a51f8a624
Akshay-R <114207199+AkshayPathakAhom@users.noreply.github.com>
2023-07-06 11:31:11 +0530

    taxType,taxRateCategory

--- src/main/java/com/mps/think/setup/model/BasicCommodityTaxRate.java
+++ src/main/java/com/mps/think/setup/model/BasicCommodityTaxRate.java
@@ -35,12 +35,29 @@ public class BasicCommodityTaxRate extends BaseEntity {
 
 	@Column(name = "expiration_date")
 	private Date expirationDate;
-	
+
 	@Column(name = "rate_name")
 	private String rateName;
 
 	@Column(name = "rate_value")
 	private String rateValue;
+
+	@Column(name = "tax_message1")
+	private String taxMessage1;
+
+	@Column(name = "tax_message2")
+	private String taxMessage2;
+
+	@Column(name = "unit_per_segment")
+	private String unitPerSegment;
+
+	@OneToOne
+	@JoinColumn(name = "basic_tax_type_id", referencedColumnName = "tax_id")
+	private TaxType taxType;
+
+	@OneToOne
+	@JoinColumn(name = "commo_tax_category_id", referencedColumnName = "id")
+	private TaxRateCategory taxRateCategory;
 
 	public Integer getBasicCommodityTaxId() {
 		return basicCommodityTaxId;
@@ -98,11 +115,53 @@ public class BasicCommodityTaxRate extends BaseEntity {
 		this.rateValue = rateValue;
 	}
 
+	public String getTaxMessage1() {
+		return taxMessage1;
+	}
+
+	public void setTaxMessage1(String taxMessage1) {
+		this.taxMessage1 = taxMessage1;
+	}
+
+	public String getTaxMessage2() {
+		return taxMessage2;
+	}
+
+	public void setTaxMessage2(String taxMessage2) {
+		this.taxMessage2 = taxMessage2;
+	}
+
+	public String getUnitPerSegment() {
+		return unitPerSegment;
+	}
+
+	public void setUnitPerSegment(String unitPerSegment) {
+		this.unitPerSegment = unitPerSegment;
+	}
+
+	public TaxType getTaxType() {
+		return taxType;
+	}
+
+	public void setTaxType(TaxType taxType) {
+		this.taxType = taxType;
+	}
+
+	public TaxRateCategory getTaxRateCategory() {
+		return taxRateCategory;
+	}
+
+	public void setTaxRateCategory(TaxRateCategory taxRateCategory) {
+		this.taxRateCategory = taxRateCategory;
+	}
+
 	@Override
 	public String toString() {
 		return "BasicCommodityTaxRate [basicCommodityTaxId=" + basicCommodityTaxId + ", basicCommodityTaxRateId="
 				+ basicCommodityTaxRateId + ", commodityCodes=" + commodityCodes + ", effectiveDate=" + effectiveDate
-				+ ", expirationDate=" + expirationDate + ", rateName=" + rateName + ", rateValue=" + rateValue + "]";
+				+ ", expirationDate=" + expirationDate + ", rateName=" + rateName + ", rateValue=" + rateValue
+				+ ", taxMessage1=" + taxMessage1 + ", taxMessage2=" + taxMessage2 + ", unitPerSegment=" + unitPerSegment
+				+ ", taxType=" + taxType + "]";
 	}
 
 }
--- src/main/java/com/mps/think/setup/vo/BasicCommodityTaxRateVO.java
+++ src/main/java/com/mps/think/setup/vo/BasicCommodityTaxRateVO.java
@@ -2,9 +2,15 @@ package com.mps.think.setup.vo;
 
 import java.util.Date;
 
-import com.mps.think.setup.model.BasicTaxRate;
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
-public class BasicCommodityTaxRateVO  {
+import com.mps.think.setup.model.BasicTaxRate;
+import com.mps.think.setup.model.TaxRateCategory;
+import com.mps.think.setup.model.TaxType;
+
+public class BasicCommodityTaxRateVO {
 
 	private Integer basicCommodityTaxId;
 	private BasicTaxRate basicCommodityTaxRateId;
@@ -14,10 +20,20 @@ public class BasicCommodityTaxRateVO  {
 	private Date effectiveDate;
 
 	private Date expirationDate;
-	
+
 	private String rateName;
 
 	private String rateValue;
+
+	private String taxMessage1;
+
+	private String taxMessage2;
+
+	private String unitPerSegment;
+
+	private TaxType taxType;
+
+	private TaxRateCategory taxRateCategory;
 
 	public Integer getBasicCommodityTaxId() {
 		return basicCommodityTaxId;
@@ -75,11 +91,53 @@ public class BasicCommodityTaxRateVO  {
 		this.rateValue = rateValue;
 	}
 
+	public String getTaxMessage1() {
+		return taxMessage1;
+	}
+
+	public void setTaxMessage1(String taxMessage1) {
+		this.taxMessage1 = taxMessage1;
+	}
+
+	public String getTaxMessage2() {
+		return taxMessage2;
+	}
+
+	public void setTaxMessage2(String taxMessage2) {
+		this.taxMessage2 = taxMessage2;
+	}
+
+	public String getUnitPerSegment() {
+		return unitPerSegment;
+	}
+
+	public void setUnitPerSegment(String unitPerSegment) {
+		this.unitPerSegment = unitPerSegment;
+	}
+
+	public TaxType getTaxType() {
+		return taxType;
+	}
+
+	public void setTaxType(TaxType taxType) {
+		this.taxType = taxType;
+	}
+
+	public TaxRateCategory getTaxRateCategory() {
+		return taxRateCategory;
+	}
+
+	public void setTaxRateCategory(TaxRateCategory taxRateCategory) {
+		this.taxRateCategory = taxRateCategory;
+	}
+
 	@Override
 	public String toString() {
 		return "BasicCommodityTaxRateVO [basicCommodityTaxId=" + basicCommodityTaxId + ", basicCommodityTaxRateId="
 				+ basicCommodityTaxRateId + ", commodityCodes=" + commodityCodes + ", effectiveDate=" + effectiveDate
-				+ ", expirationDate=" + expirationDate + ", rateName=" + rateName + ", rateValue=" + rateValue + "]";
+				+ ", expirationDate=" + expirationDate + ", rateName=" + rateName + ", rateValue=" + rateValue
+				+ ", taxMessage1=" + taxMessage1 + ", taxMessage2=" + taxMessage2 + ", unitPerSegment=" + unitPerSegment
+				+ ", taxType=" + taxType + ", taxRateCategory=" + taxRateCategory + "]";
 	}
 
 }
