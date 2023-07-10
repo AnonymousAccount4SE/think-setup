8f82456de0c42e606ed15c9ca535bd79bd1cc74b
amitkr0099 <amit.kr@mpslimited.com>
2023-07-10 16:48:13 +0530

    created gethistory by customer id

--- src/main/java/com/mps/think/setup/controller/EditTrailController.java
+++ src/main/java/com/mps/think/setup/controller/EditTrailController.java
@@ -1,6 +1,7 @@
 package com.mps.think.setup.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +13,19 @@ import com.mps.think.setup.service.EditTrailService;
 @RestController
 @CrossOrigin
 public class EditTrailController {
-	
+
 	@Autowired
 	EditTrailService editTrailService;
 
 	@GetMapping("/getHistoryByCustomerId")
 	public ResponseEntity<?> getHistoryByCustomerId(@RequestParam Integer id) {
 		return ResponseEntity.ok(editTrailService.getAllEditTrailByCustId(id));
+	}
+
+	@GetMapping("/getHistoryById")
+	public ResponseEntity<?> getHistoryById(@RequestParam Integer pubId, @RequestParam(required = false) Integer customerId,
+			@RequestParam(required = false) Integer orderId, @RequestParam(defaultValue = "0") Integer page,
+			@RequestParam(defaultValue = "5") Integer size) {
+		return ResponseEntity.ok(editTrailService.findEditTrialById(pubId, customerId, orderId, PageRequest.of(page, size)));
 	}
 }
